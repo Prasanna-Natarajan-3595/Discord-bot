94efc61aee88a5cc14953f7ff1a349d5bfabd729
Prasanna.N <prasannanatarajan3595@gmail.com>
2020-10-25 18:37:57 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -25,23 +25,27 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Music"))
+    await client.change_presence(status = discord.Status.idle, activity = discord.Activity(type=discord.ActivityType.listening, name="Alone"))
 
 
 @client.event
 async def on_message(message):
+    await client.change_presence(status = discord.Status.)
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
 
     if client.user.mentioned_in(message):
+
         msg = message.content
         msg = msg.split(' ', 1)[1]
 
         if 'newsong' in msg.lower().replace(' ', '') and var.sub == 0:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="to new song"))
             await message.channel.send(f'Name your new song {message.author.mention}')
             var.ram.clear() 
             var.sub = 1
 
         elif var.sub == 1:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="to new lyrics"))
             await message.channel.send(f'Name your new lyrics {message.author.mention}')
             var.ram.append(msg.lower())
             var.sub = 2
@@ -60,6 +64,7 @@ async def on_message(message):
                 cur.execute(f"CREATE TABLE {var.ram[0]}(id SERIAL PRIMARY KEY, {final})")
                 await message.channel.send(f'Awesome {message.author.mention} your new {var.ram[0]} song created succesfully')
                 con.commit()
+                await client.change_presence(status = discord.Status.idle, activity = discord.Activity(type=discord.ActivityType.listening, name="Friends"))
             except Exception as sa:
                 await message.channel.send(f'''{message.author.mention} an error has occured successfully
         Successfull Error:```{sa}```''')
@@ -90,16 +95,20 @@ async def on_message(message):
                 con.rollback()
 
         elif 'newbeat' in msg.lower().replace(' ', '') and var.sub == 0:
+             await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="new beat"))
              await message.channel.send(f'Enter the song name {message.author.mention}')
              var.sub=4
 
         elif var.sub == 4:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="new beat song name"))
             var.ram.clear()
             var.ram.append(msg)
             var.sub = 5
             await message.channel.send(f"Enter the {msg}'s lyrics values {message.author.mention}")
 
         elif var.sub == 5:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="new beat lyrics value"))
+             
             var.sub=0                        
             cur.execute(f"Select * FROM {var.ram[0]} LIMIT 0")
             colnames = [desc[0] for desc in cur.description]
@@ -112,6 +121,8 @@ async def on_message(message):
                 cur.execute(f"INSERT INTO {var.ram[0]}({colnames}) VALUES({res})")
                 con.commit()
                 await message.channel.send(f'{message.author.mention} {var.ram[0]} has updated successfully')
+                await client.change_presence(status = discord.Status.idle, activity = discord.Activity(type=discord.ActivityType.listening, name="moving on"))
+             
             except Exception as e:
                 await message.channel.send(f'''{message.author.mention} an error has occured successfully
         Successfull Error:```{e}```''')
@@ -140,14 +151,20 @@ async def on_message(message):
 ```{colnames}```''')
 
         elif 'deletebeat' in msg.lower().replace(' ', '') and var.sub == 0:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="delete beat"))
+             
             await message.channel.send(f'{message.author.mention} Enter song name')
             var.sub=7
             var.ram.clear()
         elif var.sub == 7:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="delete beat song name"))
+             
             await message.channel.send(f'{message.author.mention} Enter the id value')
             var.sub = 8
             var.ram.append(msg)
         elif var.sub == 8:
+            await client.change_presence(status = discord.Status.do_not_disturb, activity = discord.Activity(type=discord.ActivityType.listening, name="delete beat id"))
+             
             var.ram.append(msg)
             var.sub = 0
             try:
@@ -155,6 +172,8 @@ async def on_message(message):
                 cur.execute(f"DELETE FROM {var.ram[0]} where id = {int(var.ram[1])}")
                 con.commit()
                 await message.channel.send(f"{message.author.mention} The {var.ram[0]} beat deleted successfully")
+                await client.change_presence(status = discord.Status.idle, activity = discord.Activity(type=discord.ActivityType.listening, name="new beat"))
+             
 
             except Exception as e:
                 await message.channel.send(f'''{message.author.mention} an error has occured successfully
@@ -205,6 +224,8 @@ calculate <PROBLEM> - It finds the answer for it
 roll back - To rollback to previous state
 ```
 ''')
+        else:
+            await message.channel.send(f'{message.author.mention} why did you mention me.')
             
 
 client.run(var.token)
